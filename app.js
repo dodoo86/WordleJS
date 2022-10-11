4dc264fc1ef6f3e5d738fad87b6db4fd089a1aa5
dodoo86 <50961707+dodoo86@users.noreply.github.com>
2022-10-11 18:36:24 +0200

    keyboard added

--- app.js
+++ app.js
@@ -1,2 +1,40 @@
-// JavaScript source code
-//sanyi
+const tileDisplay = document.querySelector('.tile-container')
+const keyboard = document.querySelector('.key-container')
+
+
+const keys = [
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'ENTER',
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+    'M',
+    '<<',
+]
+
+keys.forEach(key => {
+    const buttonElement = document.createElement('button')
+    buttonElement.textContent = key
+    keyboard.append(buttonElement)
+})

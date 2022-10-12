aefa657833796ad575d69ee967404a64198c6961
dodoo86 <50961707+dodoo86@users.noreply.github.com>
2022-10-12 18:54:03 +0200

    wtf

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+const { response } = require("express")
+
 const tileDisplay = document.querySelector('.tile-container')
 const keyboard = document.querySelector('.key-container')
 const messageDisplay = document.querySelector('.message-container')
 
-const wordle = 'DAVID'
+let wordle
+
+const getWordle = () => {
+    fetch('http://localhost:8000/word')
+        .then(response => response.json())
+        .then(json => {
+            wordle = json.toUpperCase()
+        })
+        .catch(err => console.log(err))
+}
+getWordle()
 
 const keys = [
     'Q',
